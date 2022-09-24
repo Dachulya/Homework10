ab5da1995aa0b2d49bc8e5e21d6e83d39d8cf85c
Дарья <dnovogodniaya@ya.ru>
2022-09-24 20:19:47 +0300

    1branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание1
+        String firstName= "Ivanov ";
+        String middleName= "Ivan ";
+        String lastName= "Ivanovich ";
+        System.out.println("ФИО сотрудника "+firstName+middleName+lastName);
+        //Задание2
+        String fullName=firstName+middleName+lastName;
+        String hightFullName= fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+hightFullName);
+        //Задание3
+        String fullNameTwo="Иванов Семён Семёныч";
+        String fullNameTwoNew= fullNameTwo.replace('ё','е');
+        System.out.println("Данные ФИО сотрудника — "+fullNameTwoNew);
+
     }
 }
